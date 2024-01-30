20100f1d6be6cf80e9eff845f5400501a3ddf7ab
Simon Cole <simonccole1@gmail.com>
2024-01-29 22:13:17 -0500

    added output file capabilities

--- src/App.java
+++ src/App.java
@@ -1,3 +1,7 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
@@ -11,6 +15,13 @@ public class App {
         //build starting scenario from user input
         //simulate cockroach moving
         //output results
+        //The software shall output the number and direction of moves made by the cockroach in each
+        //simulation run, and the average of these simulated moves into the specified output file.
+        // The software shall also output the room size, location of the aspirin and glass of water, and
+        // number of runs into the output file
+        // The software shall format the output file with labels for each data stored to facilitate reading
+        // The software shall output upon termination of the simulation the time spent running the
+        // simulations.
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Hello, enter the width of the floor: ");
@@ -34,27 +45,61 @@ public class App {
         System.out.print("How many simulations should be run? ");
         int numSimulations = verifyNum(scanner);
 
+        System.out.println("Enter a filename for the output file with any path prepeneded to it: ");
+        String filename = scanner.next();
+
+        int roomSize = floorWidth * floorHeight;
+        writeToFile(filename, "Room size: " + roomSize);
+        String aspirinLocation = "(" + aspirinX + ", " + aspirinY + ")";
+        writeToFile(filename, "Aspirin location: " + aspirinLocation);
+        String waterLocation = "(" + waterX + ", " + waterY + ")";
+        writeToFile(filename, "Water location: " + waterLocation);
+        writeToFile(filename, "Number of simulations: " + numSimulations);
+
+        long startTime = System.nanoTime();
+
         int numSuccesses = 0;
+        ArrayList<Integer> numMovesArr = new ArrayList<Integer>();
         for(int i = 0; i < numSimulations; i++) {
             Floor floor = new Floor(floorWidth, floorHeight, new int[]{aspirinX, aspirinY}, new int[]{waterX, waterY});
 
-            if(simulate(floor)) {
+            if(simulate(floor, filename, numMovesArr)) {
                 numSuccesses++;
             }
         }
+        int avgMoves = 0;
+        for(int numMoves : numMovesArr) {
+            avgMoves += numMoves;
+        }
+        avgMoves = avgMoves / numMovesArr.size();
+        writeToFile(filename, "Average number of moves: " + avgMoves);
+        
         System.out.println("Number of successes: " + numSuccesses);
+
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime) / 1000000;
+        writeToFile(filename, "Time spent running simulations in milliseconds: " + duration);
         
 
     }
 
-    public static boolean simulate(Floor floor) {
+    public static boolean simulate(Floor floor, String filename, ArrayList<Integer> numMovesArr) {
         int numMoves = 0;
         Random rand = new Random();
+        ArrayList<String> directions = new ArrayList<String>();
         while(floor.getUnvisitedTiles().size() > 0) {
             Mover cockroach = floor.getCockroach();
             int roachX = cockroach.getX();
             int roachY = cockroach.getY();
-            // System.out.println("Cockroach coordinates: (" + roachX + ", " + roachY + ")");
+
+            //for the case the cockroach is initially on aspirin or water
+            if(!floor.getAspirin().getFound() && roachX == floor.getAspirin().getX() && roachY == floor.getAspirin().getY()) {
+                floor.getAspirin().setFound();
+            }
+            if(!floor.getAspirin().getFound() && cockroach.getX() == floor.getWater().getX() && cockroach.getY() == floor.getWater().getY()) {
+                floor.getWater().setFound();
+            }
+
             Tile currTile = floor.getTile(roachX, roachY);
 
             ArrayList<String> availableDirections = currTile.getAvailibleDirections();
@@ -65,18 +110,22 @@ public class App {
                 //top
                 case "top":
                     cockroach.decrementY();
+                    directions.add("up;");
                     break;
                 //right
                 case "right":
                     cockroach.incrementX();
+                    directions.add("right;");
                     break;
                 //bottom
                 case "bottom":
                     cockroach.incrementY();
+                    directions.add("down;");
                     break;
                 //left
                 case "left":
                     cockroach.decrementX();
+                    directions.add("left;");
                     break;
             }
             numMoves++;
@@ -96,7 +145,13 @@ public class App {
             }
 
             if(floor.getAspirin().getFound() && floor.getWater().getFound()) {
-                System.out.println("Moves to find: " + numMoves);
+                String directionsString = "";
+                for(String dir : directions) {
+                    directionsString += dir;
+                }
+                writeToFile(filename, "Directions: " + directionsString);
+                writeToFile(filename, "Number of moves: " + numMoves);
+                numMovesArr.add(numMoves);
                 return true;
             }
         }
@@ -152,6 +207,15 @@ public class App {
             else {
                 System.out.println("Please enter a number in the range of 0 to " + maxCoord + ".");
             }
+        }
+    }
+
+    public static void writeToFile(String filename, String content) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true))) {
+            writer.write(content);
+            writer.newLine(); 
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
